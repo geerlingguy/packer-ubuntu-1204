f538257c5a79887b7d59e3c97a6a647635ea9ed9
Jeff Geerling <geerlingguy@mac.com>
2016-02-24 21:31:07 -0600

    Fix Ansible installation for latest version.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,6 +4,7 @@
 rm /var/lib/apt/lists/*
 apt-get -y update && apt-get -y upgrade
 apt-get -y install software-properties-common
+apt-get -y install python-software-properties
 apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
